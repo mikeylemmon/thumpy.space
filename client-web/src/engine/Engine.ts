f463c9841b280ab55d9641db1cdf265195872d01
Mikey Lemmon <mikeylemmon@gmail.com>
2020-08-30 19:28:14 +1200

    Hack in a p5-backed video instrument
    Create a VideoOutput UI component that runs a p5 sketch defined by a
    test "Circle" EngineInstrument, and hook up the component so that the
    Circle instrument receives triggers from the same sequencer as the
    Casio. To demonstrate that triggers are working and synced with the
    audio, Circle updates the radius of a circle based on the frequency of
    the trigger.

--- client-web/src/engine/Engine.ts
+++ client-web/src/engine/Engine.ts
@@ -1,3 +1,4 @@
+import { Transport } from 'tone'
 import { StateSequence } from 'storeShared/sliceSequences'
 import { StateInstrument } from 'storeShared/sliceInstruments'
 import { EngineInstrument, EngineInstrumentStatic, EngineInstrumentTypeList } from './EngineInstrument'
@@ -9,10 +10,7 @@ export function registerInstrumentType(instType: EngineInstrumentStatic) {
 	instrumentTypes.push(instType)
 }
 
-class Engine {
-	private sequencers: { [key: string]: EngineSequence } = {}
-	private instruments: { [key: string]: EngineInstrument } = {}
-
+export class Engine {
 	static InstrumentTypes(): EngineInstrumentTypeList {
 		const list: EngineInstrumentTypeList = []
 		for (let ii = 0; ii < instrumentTypes.length; ii++) {
@@ -24,6 +22,11 @@ class Engine {
 		}
 		return list
 	}
+
+	private isAudioPlayer: boolean = false
+	private sequencers: { [key: string]: EngineSequence } = {}
+	private instruments: { [key: string]: EngineInstrument } = {}
+	private instrumentsDisabled: { [key: string]: boolean } = {}
 
 	constructor() {
 		console.log('[engine #constructor] instruments:', ...Engine.InstrumentTypes())
@@ -38,6 +41,24 @@ class Engine {
 		}
 	}
 
+	instrumentById(instId: string): EngineInstrument {
+		return this.instruments[instId]
+	}
+
+	updateIsAudioPlayer(isAudioPlayer: boolean) {
+		// // TODO:
+		// if (!isAudioPlayer && this.isAudioPlayer) {
+		// 	// Audio turned off, disconnect and remove audio instruments
+		// }
+		this.isAudioPlayer = isAudioPlayer
+		console.log('[Engine #updateIsAudioPlayer]', isAudioPlayer)
+	}
+
+	updatePaused(paused: boolean) {
+		console.log('[Engine #updatePaused]', paused ? 'stop' : 'start')
+		paused ? Transport.stop() : Transport.start()
+	}
+
 	updateInstrument(inst: StateInstrument) {
 		if (!this.instruments[inst.id]) {
 			const Inst = instrumentTypes.find(ii => ii.TypeId === inst.typeId)
@@ -46,6 +67,11 @@ class Engine {
 					'[Engine #updateInstrument] Unable to find instrument type with TypeId',
 					inst.typeId,
 				)
+				return
+			}
+			if (Inst.Subengine === 'audio' && !this.isAudioPlayer) {
+				console.log('[Engine #updateInstrument] Skipping audio instrument', inst.id)
+				this.instrumentsDisabled[inst.id] = true
 				return
 			}
 			this.instruments[inst.id] = new Inst(inst)
@@ -60,14 +86,14 @@ class Engine {
 			this.sequencers[seq.id] = new EngineSequence(seq)
 		}
 		const engSeq = this.sequencers[seq.id]
+		// TODO: Create map of outputs and provide to update call, remove connect call
 		for (const seqOut of seq.outputs) {
-			// TODO: Only update output connects when they change
+			if (this.instrumentsDisabled[seqOut.instrumentId]) {
+				continue // skip disabled instruments
+			}
 			const engInst = this.instruments[seqOut.instrumentId]
 			if (!engInst) {
-				console.error(
-					'[Engine #updateSequence] Unable to find instrument for sequence output',
-					seqOut,
-				)
+				console.warn('[Engine #updateSequence] Unable to find instrument for sequence output', seqOut)
 				continue
 			}
 			engSeq.connect(seqOut, engInst)
@@ -76,4 +102,5 @@ class Engine {
 	}
 }
 
-export default Engine
+const engine = new Engine()
+export default engine
---
+++ client-web/src/engine/video/Circle.ts
@@ -0,0 +1,69 @@
+// import * as p5 from 'p5'
+import { Draw } from 'tone'
+import { Time } from 'tone/Tone/core/type/Units'
+import { StateInstrument, StateInstrumentInput } from 'storeShared/sliceInstruments'
+import { Trigger } from 'storeShared/sliceSequences'
+import { EngineInstrument, SubengineType } from 'engine/EngineInstrument'
+import { registerInstrumentType } from 'engine/Engine'
+
+let circleId = 0
+
+// Circle statically implements EngineInstrumentStatic
+export default class Circle extends EngineInstrument {
+	static DisplayName: string = 'Circle'
+	static Subengine: SubengineType = 'video'
+	static TypeId: string = 'video-circle'
+
+	static StateInputs(): StateInstrumentInput[] {
+		return [{ id: 'trigger', name: 'Trigger' }]
+	}
+
+	static StateDefault(): StateInstrument {
+		return {
+			id: `${Circle.TypeId}-${circleId}`,
+			name: 'Circle',
+			typeId: Circle.TypeId,
+			triggers: Circle.StateInputs(),
+			fields: [],
+		}
+	}
+
+	public subengine: SubengineType = Circle.Subengine
+
+	private radius: number = 80
+
+	constructor(initialState: Partial<StateInstrument> = Circle.StateDefault()) {
+		super()
+		console.log('[Circle #constructor] New circle', initialState)
+	}
+
+	dispose(): void {
+		console.log('[Circle #dispose] Disposed')
+	}
+
+	trigger(time: Time, _inputId: string, trig: Trigger): void {
+		Draw.schedule(() => {
+			this.radius = Math.floor(Math.random() * 15 + (trig.freq - 44) * 20)
+			// console.log('[Circle #trigger] trigger:', this.radius, trig.freq)
+		}, time)
+	}
+
+	updateState(inst: StateInstrument): void {
+		console.log('[Circle #updateState] Received new state', inst)
+	}
+
+	sketch = (pp: p5) => {
+		// arrow operator so "this" works
+		pp.setup = () => {
+			console.log('[Circle #sketch.setup]')
+			pp.createCanvas(200, 200)
+		}
+
+		pp.draw = () => {
+			pp.background(200, 80, 20)
+			pp.ellipse(100, 100, this.radius, this.radius)
+		}
+	}
+}
+
+registerInstrumentType(Circle)
