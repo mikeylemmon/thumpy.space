2e51fc7cedaa9f1847e5a88c497f061fe7473b07
Mikey Lemmon <mikeylemmon@gmail.com>
2020-09-28 12:28:21 +1300

    Copy build directory to root-level "docs" folder to enable hosting on github pages

---
+++ docs/precache-manifest.5dba87f49f991f10cd159d775a9ed4e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7be7925fcc63731c72178633e8e2fed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9018164a54a78f25a7f",
+    "url": "/static/css/main.d94ee2a7.chunk.css"
+  },
+  {
+    "revision": "68ab17779d4307fd4906",
+    "url": "/static/js/2.fbb7827a.chunk.js"
+  },
+  {
+    "revision": "e58b2fed1a83725c5b0108382043ede5",
+    "url": "/static/js/2.fbb7827a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9018164a54a78f25a7f",
+    "url": "/static/js/main.5b2acb8b.chunk.js"
+  },
+  {
+    "revision": "cb27153ea0498fd9cb03",
+    "url": "/static/js/runtime-main.1f1f2664.js"
+  }
+]);
